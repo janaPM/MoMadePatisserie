50b68a935a675a8fb86cd4837239dc2aaca82a9a
Karthick Ajan <karthickajangs@gmail.com>
2026-02-17 02:39:25 +0530

    Removed the Angular Router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoMadeModule } from './mo-made.component';
 
@@ -11,7 +10,6 @@ import { MoMadeModule } from './mo-made.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     MoMadeModule
   ],
   providers: [
